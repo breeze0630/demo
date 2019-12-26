ce6da12be065299983a72b54c8fd693fabfe206d
ll2lxl <external_ll@163.com>
2019-12-26 16:49:31 +0800

    fix bug

--- middleware-demo/jdbc-demo/src/main/java/com/example/jdbc/jdbcdemo/config/SqlConfig.java
+++ middleware-demo/jdbc-demo/src/main/java/com/example/jdbc/jdbcdemo/config/SqlConfig.java
@@ -1,19 +1,15 @@
 package com.example.jdbc.jdbcdemo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.orm.hibernate5.SpringBeanContainer;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
