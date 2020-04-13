532055d20c6f4dd5681e79eb50fb082aea317a5a
ll2lxl <external_ll@163.com>
2020-04-13 11:25:55 +0800

    add learn

--- middleware-demo/spring-learn-demo/bean-lifecycle/src/main/java/com/example/middleware/springlearn/lifecycle/BeanInstantiationLifecycleDemo.java
+++ middleware-demo/spring-learn-demo/bean-lifecycle/src/main/java/com/example/middleware/springlearn/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -1,17 +1,30 @@
 package com.example.middleware.springlearn.lifecycle;
 
 
+import com.example.middleware.springlearn.lifecycle.domain.UserHolder;
 import com.example.springlearndemo.dependencylookup.domain.SuperUser;
 import com.example.springlearndemo.dependencylookup.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.ObjectUtils;
 
 public class BeanInstantiationLifecycleDemo {
 
     public static void main(String[] args) {
+        executeBeanFactory();
+        executeApplicationContext();
+    }
+
+    /**
+     * DefaultListableBeanFactory 此方法没有回调 environment，
+     * EnvironmentAware 接口是和应用上下文相关的回调
+     * */
+    private static void executeBeanFactory(){
         DefaultListableBeanFactory beanFactory  = new DefaultListableBeanFactory();
 
         beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
@@ -28,29 +41,33 @@ public class BeanInstantiationLifecycleDemo {
 
         System.out.println(superUser);
 
+        UserHolder userHolder = beanFactory.getBean("userHolder",UserHolder.class);
 
+        System.out.println(userHolder);
     }
+    private static void executeApplicationContext(){
+        System.out.println("----executeApplicationContext----");
+        ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext();
 
-    static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
-        @Override
-        public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        //用 Xml注册Bean MyInstantiationAwareBeanPostProcessor
 
-            if(ObjectUtils.nullSafeEquals( "superUser",beanName) && SuperUser.class.equals(beanClass)){
-                return  new SuperUser();
-            }
-            return null; //如果返回Null，保持之前的处理过程不变,采用默认的实例化方式
-        }
+        String[] location = {"classpath:/META-INF/dependency-lookup-context.xml","classpath:/META-INF/bean-constructor-dependency-injection.xml"};
+        applicationContext.setConfigLocations(location);
 
-        @Override
-        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-            if(ObjectUtils.nullSafeEquals("user",beanName) ){
-                User user = (User) bean;
-                user.setId(999L);
-//                user.setName("测试");
-                return false;
-            }
-            return true;
-        }
+        applicationContext.refresh();
+        User user = applicationContext.getBean("user",User.class);
+
+        System.out.println(user);
+
+        SuperUser superUser = applicationContext.getBean("superUser",SuperUser.class);
+
+        System.out.println(superUser);
+
+        UserHolder userHolder = applicationContext.getBean("userHolder",UserHolder.class);
+
+        System.out.println(userHolder);
+        applicationContext.close();
     }
 
 }
+
--- middleware-demo/spring-learn-demo/bean-lifecycle/src/main/java/com/example/middleware/springlearn/lifecycle/domain/UserHolder.java
+++ middleware-demo/spring-learn-demo/bean-lifecycle/src/main/java/com/example/middleware/springlearn/lifecycle/domain/UserHolder.java
@@ -2,10 +2,36 @@ package com.example.middleware.springlearn.lifecycle.domain;
 
 
 import com.example.springlearndemo.dependencylookup.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
-public class UserHolder {
+public class UserHolder implements EnvironmentAware , BeanNameAware, BeanClassLoaderAware, BeanFactoryAware {
 
     private  User user ;
+
+    private int number;
+    private String description;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public UserHolder() {
     }
@@ -19,6 +45,68 @@ public class UserHolder {
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
+    }
+
+    /**
+     * Set the {@code Environment} that this component runs in.
+     *
+     * @param environment
+     */
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println("回调 environment 方法");
+    }
+
+    /**
+     * Callback that supplies the bean {@link ClassLoader class loader} to
+     * a bean instance.
+     * <p>Invoked <i>after</i> the population of normal bean properties but
+     * <i>before</i> an initialization callback such as
+     * {@link InitializingBean InitializingBean's}
+     * {@link InitializingBean#afterPropertiesSet()}
+     * method or a custom init-method.
+     *
+     * @param classLoader the owning class loader
+     */
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("回调 classLoader");
+    }
+
+    /**
+     * Callback that supplies the owning factory to a bean instance.
+     * <p>Invoked after the population of normal bean properties
+     * but before an initialization callback such as
+     * {@link InitializingBean#afterPropertiesSet()} or a custom init-method.
+     *
+     * @param beanFactory owning BeanFactory (never {@code null}).
+     *                    The bean can immediately call methods on the factory.
+     * @throws BeansException in case of initialization errors
+     * @see BeanInitializationException
+     */
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("回调 beanFactory");
+    }
+
+    /**
+     * Set the name of the bean in the bean factory that created this bean.
+     * <p>Invoked after population of normal bean properties but before an
+     * init callback such as {@link InitializingBean#afterPropertiesSet()}
+     * or a custom init-method.
+     *
+     * @param name the name of the bean in the factory.
+     *             Note that this name is the actual bean name used in the factory, which may
+     *             differ from the originally specified name: in particular for inner bean
+     *             names, the actual bean name might have been made unique through appending
+     *             "#..." suffixes. Use the {@link BeanFactoryUtils#originalBeanName(String)}
+     *             method to extract the original bean name (without suffix), if desired.
+     */
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("回调 BeanName");
     }
 }
