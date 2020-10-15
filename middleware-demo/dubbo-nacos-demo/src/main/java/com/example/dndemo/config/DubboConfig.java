ee881f609a47bdc4e10be8ca46f31d1a91ff6741
breeze0630 <external_ll@163.com>
2020-10-15 23:35:12 +0800

    add test

--- middleware-demo/dubbo-nacos-demo/src/main/java/com/example/dndemo/config/DubboConfig.java
+++ middleware-demo/dubbo-nacos-demo/src/main/java/com/example/dndemo/config/DubboConfig.java
@@ -1,8 +1,15 @@
 package com.example.dndemo.config;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDubbo
+        (scanBasePackages = "com.example.dndemo")
+//@EnableDubboConfig
 public class DubboConfig {
 
     private String port;
