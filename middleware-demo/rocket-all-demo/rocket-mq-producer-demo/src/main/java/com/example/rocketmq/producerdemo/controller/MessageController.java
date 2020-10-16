80238a9fcd21fd083179eb8f44f200f63f1d7bd6
breeze <external_ll@163.com>
2020-10-16 21:01:39 +0800

    add rocketmq's demo

--- middleware-demo/rocket-all-demo/rocket-mq-producer-demo/src/main/java/com/example/rocketmq/producerdemo/controller/MessageController.java
+++ middleware-demo/rocket-all-demo/rocket-mq-producer-demo/src/main/java/com/example/rocketmq/producerdemo/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.rocketmq.producerdemo.controller;
 
 import com.example.rocketmq.producerdemo.dto.MessageDTO;
+import com.example.rocketmq.producerdemo.producer.AsyncProducer;
 import com.example.rocketmq.producerdemo.producer.SyncProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,20 @@ public class MessageController {
     private SyncProducer syncProducer;
 
 
+    @Autowired
+    private AsyncProducer asyncProducer;
+
     @GetMapping(value = "/send")
     public String send(String topic,String tag,String message) throws Exception{
         MessageDTO messageDTO = new MessageDTO(topic,tag,message);
         syncProducer.sendMsg(messageDTO);
+        return "success";
+    }
+
+    @GetMapping(value = "/sendAsync")
+    public String sendAsync(String topic,String tag,String message) throws Exception{
+        MessageDTO messageDTO = new MessageDTO(topic,tag,message);
+        asyncProducer.send(messageDTO);
         return "success";
     }
 }
