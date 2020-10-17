6336c896dbf51cdb3eeeb898bb71bd53daace0da
breeze0630 <external_ll@163.com>
2020-10-17 11:04:35 +0800

    add properties's demo

---
+++ middleware-demo/spring-learn-demo/annotation/src/main/java/com/example/springlearndemo/annotation/PropertyPlaceHolderDemo.java
@@ -0,0 +1,17 @@
+package com.example.springlearndemo.annotation;
+
+import com.example.springlearndemo.iocdemo.container.bean.User;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PropertyPlaceHolderDemo {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/properties-context.xml");
+
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+
+        context.close();
+    }
+}
