1e8715a064cab55ec2509bfdf500193870947722
liub1224 <894680118@qq.com>
2021-03-30 17:40:52 +0800

    rocketmq's demo

---
+++ middleware-demo/rocket-all-demo/rocket-mq-producer-demo/src/main/java/com/example/rocketmq/producerdemo/controller/ApiController.java
@@ -0,0 +1,19 @@
+package com.example.rocketmq.producerdemo.controller;
+
+import com.example.rocketmq.producerdemo.msg.Resources;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ApiController {
+
+    @GetMapping("info")
+    public String info(String msg){
+      log.info(msg);
+        String name = Resources.getMessage(msg);
+        log.info(name);
+        return name + " " + msg;
+    }
+}
