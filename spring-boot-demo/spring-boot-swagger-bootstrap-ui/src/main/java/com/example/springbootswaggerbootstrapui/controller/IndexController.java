53edfc288af5cef36d25977338e5ce361bbd436b
breeze0630 <external_ll@163.com>
2021-05-07 17:18:05 +0800

    集成knife 代码

---
+++ spring-boot-demo/spring-boot-swagger-bootstrap-ui/src/main/java/com/example/springbootswaggerbootstrapui/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.example.springbootswaggerbootstrapui.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: liubiao
+ * @date: 2021/5/7 16:34
+ * @Description:
+ */
+@RestController
+@RequestMapping("/api1")
+@Api(tags = "首页模块")
+public class IndexController {
+
+    @ApiImplicitParam(name = "name",value = "姓名",required = true)
+    @ApiOperation(value = "向客人问好")
+    @GetMapping("/sayHi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        return ResponseEntity.ok("Hi:"+name);
+    }
+}
