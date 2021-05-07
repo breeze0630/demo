a86480891aa425d202b4ad3147d4c9a48a97e7ca
breeze0630 <external_ll@163.com>
2021-05-07 16:27:49 +0800

    create module

---
+++ spring-boot-demo/spring-boot-swagger-bootstrap-ui/src/main/java/com/example/springbootswaggerbootstrapui/SpringBootSwaggerBootstrapUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggerbootstrapui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerBootstrapUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerBootstrapUiApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo/spring-boot-swagger-bootstrap-ui/src/test/java/com/example/springbootswaggerbootstrapui/SpringBootSwaggerBootstrapUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggerbootstrapui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwaggerBootstrapUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
