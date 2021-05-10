f550536ff9752a734b753cd5d8709ce74803b89e
liub1224 <894680118@qq.com>
2021-05-10 09:25:09 +0800

    add demo

---
+++ middleware-demo/serializable-demo/serializeble-bean-demo/src/main/java/com/example/serializeblebeandemo/bean/User.java
@@ -0,0 +1,11 @@
+package com.example.serializeblebeandemo.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+
+    private String name;
+
+    private int age;
+}
---
+++ middleware-demo/serializable-demo/serializeble-bean-demo/src/test/java/com/example/serializeblebeandemo/SerializebleBeanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serializeblebeandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializebleBeanDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
