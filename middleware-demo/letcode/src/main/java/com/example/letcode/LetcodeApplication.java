0bce497f5532585507e652ef74f10a8ed049a037
liub1224 <894680118@qq.com>
2021-08-10 10:48:04 +0800

    add letcode's modules

---
+++ middleware-demo/letcode/src/main/java/com/example/letcode/LetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.letcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetcodeApplication.class, args);
+    }
+
+}
