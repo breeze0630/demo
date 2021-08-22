a6ae4009a0dcc1c908224db03ee9160532e245f8
liub1224 <external_ll@163.com>
2021-08-22 20:47:52 +0800

    add leetcode's code

--- middleware-demo/letcode/src/main/java/com/example/letcode/hot8/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/letcode/hot8/Solution.java
@@ -1,6 +1,9 @@
 package com.example.letcode.hot8;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: liubiao
  * @date: 2021/8/22 10:20
@@ -10,7 +13,9 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(Integer.MIN_VALUE);
-        System.out.println(new Solution().myAtoi("-2147483647"));
+        System.out.println(new Solution().myAtoi("-42 "));
+        System.out.println(new Solution().myAtoi2("-42 "));
+
     }
     public int myAtoi(String s) {
 
@@ -23,45 +28,90 @@ public class Solution {
         int index = 0;
         boolean isStart = true;
         Boolean posi = null;
-
-        while (index < chars.length){
-
+        while (index < chars.length) {
             //去除前导空格
             char current = chars[index];
-            if(isStart && current == 32){
+            if (isStart && current == ' ') {
                 index++;
                 continue;
-            }else if(isStart && current != 32){
+            } else if (isStart && current != ' ') {
                 isStart = false;
             }
-
             //有效字符第一位
-            if(posi ==  null){
-                if(current == 43){
+            if (posi == null) {
+                if (current == 43) {
                     posi = true;
-                }else if(current == 45){
+                } else if (current == 45) {
                     posi = false;
-                }else if(current >=48 && current <= 57){
+                } else if (current >= 48 && current <= 57) {
                     posi = true;
                     result = current - 48;
-                }else {
+                } else {
                     return result;
                 }
-            }else if(current < 48 || current > 57){
+            } else if (current < 48 || current > 57) {
                 return result;
-            }else {
-                if(result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && (current-48) > Integer.MAX_VALUE %10)  ) {
+            } else {
+                if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && (current - 48) > Integer.MAX_VALUE % 10)) {
                     return Integer.MAX_VALUE;
-                }else if(result < Integer.MIN_VALUE / 10 || (result ==  Integer.MIN_VALUE / 10 && (current-48) > -1*(Integer.MIN_VALUE %10))){
+                } else if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && (current - 48) > -1 * (Integer.MIN_VALUE % 10))) {
                     return Integer.MIN_VALUE;
                 }
-                result = posi ? (result * 10 )+ (current - 48) : ( result * 10) - (current-48);
-
+                result = posi ? (result * 10) + (current - 48) : (result * 10) - (current - 48);
             }
-            index ++ ;
+            index++;
         }
-
-
         return result;
+    }
+
+    public int myAtoi2(String str) {
+        Automaton automaton = new Automaton();
+        int length = str.length();
+        for (int i = 0; i < length; ++i) {
+            automaton.get(str.charAt(i));
+        }
+        return (int) (automaton.sign * automaton.ans);
+    }
+
+
+}
+
+
+
+
+
+//DFA 解法，根据字符串的传入，逐渐修改状态，直到结束 end
+class Automaton {
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    private Map<String, String[]> table = new HashMap<String, String[]>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});
+    }};
+
+    public void get(char c) {
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
+        }
+    }
+
+    private int get_col(char c) {
+        if (c == ' ') {
+            return 0;
+        }
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        if (Character.isDigit(c)) {
+            return 2;
+        }
+        return 3;
     }
 }
