7bf4216636833bb7d8374123dfaa3897e9c063da
liub1224 <894680118@qq.com>
2021-08-30 14:08:49 +0800

    add leetcode

--- middleware-demo/letcode/src/main/java/com/example/letcode/hot11/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/letcode/hot11/Solution.java
@@ -22,4 +22,20 @@ public class Solution {
 
         return max;
     }
+
+    public int maxArea1(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
 }
