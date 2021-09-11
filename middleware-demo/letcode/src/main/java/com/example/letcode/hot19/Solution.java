f02c37e85ea1fd069a6f1f27d7f70dcea5068fc2
liub1224 <external_ll@163.com>
2021-09-11 16:07:28 +0800

    add leetcode's code

---
+++ middleware-demo/letcode/src/main/java/com/example/letcode/hot19/Solution.java
@@ -0,0 +1,46 @@
+package com.example.letcode.hot19;
+
+/**
+ * @author: liubiao
+ * @date: 2021/9/11 15:26
+ * @Description:
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        int current = 0;
+
+        return null;
+    }
+
+    public ListNode removeInner(ListNode head, ListNode first,int n,int current){
+        if(current == n){
+
+        }
+        return first;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
