baf72df63c4dc3518a0a28a313960d7923bdd727
liub1224 <894680118@qq.com>
2021-09-13 15:23:00 +0800

    add leetcode

--- middleware-demo/letcode/src/main/java/com/example/letcode/hot20/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/letcode/hot20/Solution.java
@@ -1,5 +1,7 @@
 package com.example.letcode.hot20;
 
+import java.util.LinkedList;
+
 /**
  * @author: liubiao
  * @date: 2021/9/11 16:28
@@ -9,7 +11,31 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        System.out.println(new Solution().isValid("}{"));
+    }
+    public boolean isValid(String s) {
+        if(s.length() % 2 == 1){
+            return false;
+        }
+        LinkedList<Character> characters = new LinkedList<>();
+        for( char c : s.toCharArray()){
+            if(characters.size() <= 0){
+                characters.add(c);
+                continue;
+            }else{
+                char c2 = characters.pollLast();
+                if(!isPattern(c2,c)){
+                    characters.add(c2);
+                    characters.add(c);
+                }
+
+            }
+        }
+        return characters.size() == 0;
     }
 
+    public boolean isPattern(char left,char right){
+        return  (left == '(' && right == ')' ) || (left == '[' && right == ']' ) || (left == '{' && right == '}' );
+    }
 
 }
