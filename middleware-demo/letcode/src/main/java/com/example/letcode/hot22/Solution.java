53bb8b65cad7a6cfef31b4328696b25c6743969f
liub1224 <894680118@qq.com>
2021-09-29 16:45:50 +0800

    add leetcode

---
+++ middleware-demo/letcode/src/main/java/com/example/letcode/hot22/Solution.java
@@ -0,0 +1,37 @@
+package com.example.letcode.hot22;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(JSON.toJSONString(new Solution().generateParenthesis(2)));
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs("",n,n,res);
+        return res;
+    }
+
+
+    public void dfs(String input , int left ,int right ,List<String> res)
+    {
+        if(left>right){
+            return;
+        }
+        if(left == 0 && right == 0){
+            res.add(input);
+            return;
+        }
+        if( left > 0 ){
+            dfs(input+"(",left -1,right,res);
+        }
+        if( right > 0){
+            dfs(input+")", left,right-1,res);
+        }
+    }
+
+}
