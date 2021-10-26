3437d1cd06f9d7f2ed983fe2c19b2d77eb441c7f
liub1224 <external_ll@163.com>
2021-10-26 21:42:41 +0800

    add order-hystrix code

---
+++ spring-cloud-demo/spring-cloud-demo1/hystrix-dashboard-9007/src/main/java/com/example/hystrixdashboard9007/HystrixDashboard9007Application.java
@@ -0,0 +1,39 @@
+package com.example.hystrixdashboard9007;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableHystrix
+public class HystrixDashboard9007Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9007Application.class, args);
+    }
+
+
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无关，SpringCloud本身无关，
+     * ServletRegistrationBean因为springboot的默认路径不是 /hystrix.stream
+     *
+     *
+     * @return
+     */
+/*    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }*/
+}
---
+++ spring-cloud-demo/spring-cloud-demo1/hystrix-dashboard-9007/src/test/java/com/example/hystrixdashboard9007/HystrixDashboard9007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hystrixdashboard9007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboard9007ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
