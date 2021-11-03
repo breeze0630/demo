3b4d489cf366d34bb2bc75b81d92f9aa8a25ff35
liub1224 <external_ll@163.com>
2021-11-03 21:53:12 +0800

    add springcloudconfig code

--- spring-cloud-demo/spring-cloud-demo1/cloud-order-nacos-9011/src/main/java/com/example/cloudordernacos9011/controller/OrderController.java
+++ spring-cloud-demo/spring-cloud-demo1/cloud-order-nacos-9011/src/main/java/com/example/cloudordernacos9011/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.example.apicommons.domain.common.CommonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("order")
 @Slf4j
+@RefreshScope
 public class OrderController {
 
     @Autowired
@@ -24,6 +26,15 @@ public class OrderController {
 
     @Value("${service-url.nacos-user-service}")
     private String userServiceUrl;
+
+    @Value("${order.info}")
+    private String orderInfo;
+
+    @GetMapping("getOrderInfoLocal")
+    public Object getOrderInfoLocal(){
+        return orderInfo;
+    }
+
 
     @GetMapping("getLocalInfo")
     public Object getLocalInfo(){
