372d4d5d4e1221982f8be6a3a9aeec9939ab32a6
breeze0630 <external_ll@163.com>
2021-11-10 17:40:18 +0800

    add spring cloud alibaba's code

--- spring-cloud-demo/spring-cloud-demo1/payment-service-sentinel-8060/src/main/java/com/example/paymentservicesentinel8060/controller/PaymentController.java
+++ spring-cloud-demo/spring-cloud-demo1/payment-service-sentinel-8060/src/main/java/com/example/paymentservicesentinel8060/controller/PaymentController.java
@@ -1,22 +1,41 @@
 package com.example.paymentservicesentinel8060.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class PaymentController {
 
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+    private AtomicInteger atomicInteger2 = new AtomicInteger(0);
+
     @GetMapping("/get/{id}")
+    @SentinelResource("hello")
     public Object getId(@PathVariable("id") int id){
+        atomicInteger.addAndGet(1);
         return "get:" + UUID.randomUUID() +  ":" + id;
     }
+    @SentinelResource("hello")
     @GetMapping("/getInfo/{id}")
     public Object getInfo(@PathVariable("id") int id){
+        atomicInteger2.addAndGet(1);
         return "getInfo:" + UUID.randomUUID() +  ":" + id;
     }
 
+//    @SentinelResource("hello")
+    @GetMapping("/get1")
+    public Object get1(){
+        return atomicInteger.get();
+    }
 
+//    @SentinelResource("hello")
+    @GetMapping("/get2")
+    public Object get2(){
+        return atomicInteger2.get();
+    }
 }
