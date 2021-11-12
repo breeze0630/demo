0cdbf2fb68ab9a8fb6e7ddf0f4af76804a5fa4d7
breeze0630 <external_ll@163.com>
2021-11-12 17:06:57 +0800

    add spring cloud alibaba's code

--- spring-cloud-demo/spring-cloud-demo1/payment-service-sentinel-8060/src/main/java/com/example/paymentservicesentinel8060/controller/PaymentController.java
+++ spring-cloud-demo/spring-cloud-demo1/payment-service-sentinel-8060/src/main/java/com/example/paymentservicesentinel8060/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.example.paymentservicesentinel8060.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,20 @@ public class PaymentController {
     private AtomicInteger atomicInteger2 = new AtomicInteger(0);
 
     @GetMapping("/get/{id}")
-    @SentinelResource("hello")
+    @SentinelResource(value = "hello",fallback = "helloBlockHandler")
     public Object getId(@PathVariable("id") int id){
         atomicInteger.addAndGet(1);
         return "get:" + UUID.randomUUID() +  ":" + id;
     }
-    @SentinelResource("hello")
+    @SentinelResource(value = "hello",fallback = "helloBlockHandler")
     @GetMapping("/getInfo/{id}")
     public Object getInfo(@PathVariable("id") int id){
         atomicInteger2.addAndGet(1);
         return "getInfo:" + UUID.randomUUID() +  ":" + id;
+    }
+
+    public Object helloBlockHandler(FlowException be){
+        return "异常终止!!!";
     }
 
 //    @SentinelResource("hello")
