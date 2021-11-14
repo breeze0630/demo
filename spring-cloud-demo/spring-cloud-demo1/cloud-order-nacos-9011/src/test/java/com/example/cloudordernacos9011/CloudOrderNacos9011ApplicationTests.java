207a7dd1088a1d819f33362001717b653be0e726
breeze0630 <external_ll@163.com>
2021-11-14 22:28:11 +0800

    add sentinel demo's code

--- spring-cloud-demo/spring-cloud-demo1/cloud-order-nacos-9011/src/test/java/com/example/cloudordernacos9011/CloudOrderNacos9011ApplicationTests.java
+++ spring-cloud-demo/spring-cloud-demo1/cloud-order-nacos-9011/src/test/java/com/example/cloudordernacos9011/CloudOrderNacos9011ApplicationTests.java
@@ -3,11 +3,23 @@ package com.example.cloudordernacos9011;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CloudOrderNacos9011ApplicationTests {
 
     @Test
     void contextLoads() {
+
+        long windowTime = System.currentTimeMillis();
+        System.out.println(windowTime);
+        System.out.println( windowTime%500);
+
+        System.out.println(windowTime - windowTime%500);
+
+
+        long timeId = windowTime / 500;
+
+        System.out.println(timeId);
+        System.out.println((int)(timeId % 2));
     }
 
 }
