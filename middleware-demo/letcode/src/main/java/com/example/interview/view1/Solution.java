9d353de7ac979706c0fd354de8010ef87c07698c
breeze0630 <external_ll@163.com>
2022-02-10 17:40:51 +0800

    add pom's code

---
+++ middleware-demo/letcode/src/main/java/com/example/interview/view1/Solution.java
@@ -0,0 +1,28 @@
+package com.example.interview.view1;
+
+import java.util.*;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isUnique("leetcode"));
+        System.out.println(new Solution().isUnique("abc"));
+
+    }
+
+    public boolean isUnique(String astr) {
+
+        List<Character> charSet = new ArrayList<>();
+        char[] chars = astr.toCharArray();
+        int i = 0;
+        while (i<chars.length){
+            if(charSet.contains(chars[i])){
+                return false;
+            }
+            charSet.add(chars[i]);
+            i++;
+        }
+        return true;
+    }
+}
