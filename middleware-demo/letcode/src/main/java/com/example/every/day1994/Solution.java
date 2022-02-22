a6fbab1fed11afa46884c919041286e62379e3e3
breeze0630 <external_ll@163.com>
2022-02-22 17:30:40 +0800

    add leetcode's code

---
+++ middleware-demo/letcode/src/main/java/com/example/every/day1994/Solution.java
@@ -0,0 +1,16 @@
+package com.example.every.day1994;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public int numberOfGoodSubsets(int[] nums) {
+
+        Set<Integer> zhishu = new HashSet<>();
+        for ( int i = 0; i < nums.length;i++){
+        }
+
+        return -1;
+    }
+}
