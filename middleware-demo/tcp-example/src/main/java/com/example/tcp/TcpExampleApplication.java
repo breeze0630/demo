b929d8bc15c2dfa3ae5da9e7b4822f5a09132763
breeze0630 <external_ll@163.com>
2022-06-20 10:52:35 +0800

    add test's code

---
+++ middleware-demo/tcp-example/src/main/java/com/example/tcp/TcpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpExampleApplication.class, args);
+    }
+
+}
---
+++ middleware-demo/tcp-example/src/main/java/com/example/tcp/TcpHandler.java
@@ -0,0 +1,14 @@
+package com.example.tcp;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class TcpHandler implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+
+    }
+}
