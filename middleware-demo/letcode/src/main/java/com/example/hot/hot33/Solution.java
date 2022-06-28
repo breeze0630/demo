cee1a569d845030d0b574b80076d13d5980a3247
breeze0630 <external_ll@163.com>
2022-06-28 17:29:38 +0800

    add test's code

--- middleware-demo/letcode/src/main/java/com/example/hot/hot33/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot33/Solution.java
@@ -4,18 +4,41 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println( new Solution().search(new int[]{1,3},1));
+        System.out.println( new Solution().search2(new int[]{4,5,6,7,0,1,2},3));
     }
 
+    // 最直接的解题思路便是遍历
     public int search(int[] nums, int target) {
 
         int i = 0, len = nums.length;
-        while (i < len - 1){
-            if( nums[i] > nums[i+1] && nums[i+1] == target){
-                return i+1;
+        while (i <= len - 1){
+            if(nums[i] == target){
+                return i;
             }
             i++;
         }
-        return nums[len-1] == target ? len-1 : -1;
+        return -1;
+    }
+
+    public int search2(int[] nums,int target){
+
+        int left = 0 , right = nums.length - 1;
+        while ( left <= right){
+            if(target == nums[left]){
+                return left;
+            }else if( target == nums[right]){
+                return right;
+            }else if( target < nums[left] && target > nums[right]){
+                return -1;
+            }
+
+            if( target > nums[left]){
+                left++;
+            }
+            if( target < nums[right]){
+                right--;
+            }
+        }
+        return -1;
     }
 }
