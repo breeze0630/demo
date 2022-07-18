6ffe428fc0a18c2b82f2efb0f7eb8cb4b3df872f
breeze0630 <external_ll@163.com>
2022-07-18 21:56:31 +0800

    hot 46

--- middleware-demo/letcode/src/main/java/com/example/hot/hot46/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot46/Solution.java
@@ -1,12 +1,50 @@
 package com.example.hot.hot46;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        System.out.println(new Solution().permute(new int[]{1,2,3}));
+    }
+
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
 
+        if(nums == null || nums.length == 0){
+            return res;
+        }
 
-        return null;
+        if( nums.length == 1){
+            res.add(Arrays.asList(nums[0]));
+            return res;
+        }
+        boolean[] used = new boolean[nums.length];
+
+        dfs(nums,new ArrayList(),res,used,0);
+        return res;
+    }
+
+    private void dfs(int[] nums,List<Integer> added , List<List<Integer>> res , boolean[] used,int len){
+        if(len  == nums.length){
+            List<Integer> news = added.stream().collect(Collectors.toList());
+            res.add(news);
+            return;
+        }
+
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( !used[i] ){
+                added.add(nums[i]) ;
+                used[i] = true;
+                dfs(nums,added,res,used,len+1);
+                //清除当前节点(撤销选择)
+                used[i] = false;
+                added.remove(added.size()-1);
+            }
+        }
     }
 }
