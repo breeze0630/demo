8764fab5bfb5000e00eea2397124253ec0f78ded
breeze0630 <external_ll@163.com>
2023-04-11 11:44:47 +0800

    test

---
+++ spring-cloud-demo/spring-cloud-nacos-demo/payment-service-9006/src/main/java/com/example/paymentservice9006/PaymentService9006Application.java
@@ -0,0 +1,13 @@
+package com.example.paymentservice9006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentService9006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentService9006Application.class, args);
+    }
+
+}
---
+++ spring-cloud-demo/spring-cloud-nacos-demo/payment-service-9006/src/test/java/com/example/paymentservice9006/PaymentService9006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentservice9006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentService9006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
