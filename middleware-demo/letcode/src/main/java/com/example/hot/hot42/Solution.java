ac810808e12653114586486e2bc99d9cea4e5182
breeze0630 <external_ll@163.com>
2023-04-19 10:00:43 +0800

    第42题

--- middleware-demo/letcode/src/main/java/com/example/hot/hot42/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot42/Solution.java
@@ -4,7 +4,29 @@ public class Solution {
 
     public int trap(int[] height) {
 
-        return -1;
+        int total = 0;
+
+        int temp = 0;
+        int left  = 0;
+        int right = 0;
+
+        while ( left == height.length ){
+            if(height[left] == 0 ){
+                left = right + 1;
+                right = left;
+                continue;
+            }
+            if(height[left] < height[right] && right - left == 1){
+                left = right + 1;
+                right = left;
+                continue;
+            }
+
+
+
+        }
+
+        return total;
     }
 
 
