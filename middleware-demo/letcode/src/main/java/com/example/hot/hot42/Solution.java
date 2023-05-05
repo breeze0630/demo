e8a5d76d77220353c5610910ac395330e7fdac6a
breeze0630 <external_ll@163.com>
2023-05-05 12:20:34 +0800

    leetcode

--- middleware-demo/letcode/src/main/java/com/example/hot/hot42/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot42/Solution.java
@@ -2,29 +2,31 @@ package com.example.hot.hot42;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        System.out.println(new Solution().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(new Solution().trap(new int[]{4,2,0,3,2,5}));
+
+    }
+
     public int trap(int[] height) {
 
         int total = 0;
 
-        int temp = 0;
-        int left  = 0;
-        int right = 0;
-
-        while ( left == height.length ){
-            if(height[left] == 0 ){
-                left = right + 1;
-                right = left;
-                continue;
-            }
-            if(height[left] < height[right] && right - left == 1){
-                left = right + 1;
-                right = left;
-                continue;
-            }
-
-
-
+        int[] left  = new int[height.length];
+        left[0] = height[0];
+        int[] right = new int[height.length];
+        for(int i = 1 ; i < height.length ; i++){
+            left[i] = Math.max(left[i-1],height[i]);
         }
+        right[height.length-1] = height[height.length-1];
+        for(int i = height.length-2 ; i >= 0  ; i--){
+            right[i] = Math.max(right[i+1],height[i]);
+        }
+
+        for( int i = 0; i<height.length;i++ ){
+            total +=  Math.min(left[i],right[i]) - height[i];
+        }
+
 
         return total;
     }
