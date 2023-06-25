7c80333e1d88c0c3d26a3f2ca3701ddf704a920f
breeze0630 <external_ll@163.com>
2023-06-25 22:37:23 +0800

    hot 48

--- middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
@@ -1,5 +1,7 @@
 package com.example.hot.hot48;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * @description:
  * @auther: liubiao
@@ -8,11 +10,26 @@ package com.example.hot.hot48;
 public class Solution {
 
     public static void main(String[] args) {
-
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        new Solution().rotate(matrix);
+        System.out.println(JSON.toJSONString(matrix));
     }
 
     //解题
     public void rotate(int[][] matrix) {
-
+        int n = matrix.length-1;
+        int mid = (matrix.length + 1) / 2;
+        for (int l = 0; l < mid; l++) {
+            for(int r = 0; r < mid;r++){
+                int l1 = matrix[l][r];
+                int l2 = matrix[l][n-r];
+                int r1 = matrix[n-l][r];
+                int r2 = matrix[n-l][n-r];
+                matrix[l][r] = r1;
+                matrix[n-l][r]= r2;
+                matrix[n-l][n-r] = l2;
+                matrix[l][n-r] = l1;
+            }
+        }
     }
 }
