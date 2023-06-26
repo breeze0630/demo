61f44e02f8b707e67a7fa8ec166fcb550241b8cb
breeze0630 <external_ll@163.com>
2023-06-26 11:02:37 +0800

    add hot 48's level

--- middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
@@ -11,24 +11,31 @@ public class Solution {
 
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        new Solution().rotate(matrix);
+        int[][] matrix2 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+
+        Solution s = new Solution();
+        s.rotate(matrix);
+        s.rotate(matrix2);
+
+        System.out.println(3/2);
         System.out.println(JSON.toJSONString(matrix));
+        System.out.println(JSON.toJSONString(matrix2));
+
     }
 
     //解题
     public void rotate(int[][] matrix) {
-        int n = matrix.length-1;
-        int mid = (matrix.length + 1) / 2;
-        for (int l = 0; l < mid; l++) {
-            for(int r = 0; r < mid;r++){
+        int n = matrix.length - 1;
+        for (int l = 0; l < n/2 ; l++) {
+            for (int r = 0; r < ( n + 1 )/ 2; r++) {
                 int l1 = matrix[l][r];
-                int l2 = matrix[l][n-r];
-                int r1 = matrix[n-l][r];
-                int r2 = matrix[n-l][n-r];
-                matrix[l][r] = r1;
-                matrix[n-l][r]= r2;
-                matrix[n-l][n-r] = l2;
-                matrix[l][n-r] = l1;
+                int l2 = matrix[r][n - l];
+                int l3 = matrix[n - l][n - r];
+                int l4 = matrix[n - r][l];
+                matrix[l][r] = l4;
+                matrix[r][n - l] = l1;
+                matrix[n - l][n - r] = l2;
+                matrix[n - r][l] = l3;
             }
         }
     }
