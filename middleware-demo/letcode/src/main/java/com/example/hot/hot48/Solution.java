e7a26b43a41b211decd5f167a1604529eac721f9
breeze0630 <external_ll@163.com>
2023-06-30 09:38:48 +0800

    add hot 48's solution

--- middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot48/Solution.java
@@ -10,14 +10,14 @@ import com.alibaba.fastjson.JSON;
 public class Solution {
 
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        int[][] matrix2 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
 
         Solution s = new Solution();
-        s.rotate(matrix);
-        s.rotate(matrix2);
+        s.rotate2(matrix);
+        s.rotate2(matrix2);
 
-        System.out.println(3/2);
+        System.out.println(3 / 2);
         System.out.println(JSON.toJSONString(matrix));
         System.out.println(JSON.toJSONString(matrix2));
 
@@ -26,8 +26,8 @@ public class Solution {
     //解题
     public void rotate(int[][] matrix) {
         int n = matrix.length - 1;
-        for (int l = 0; l < n/2 ; l++) {
-            for (int r = 0; r < ( n + 1 )/ 2; r++) {
+        for (int l = 0; l < n / 2; l++) {
+            for (int r = 0; r < (n + 1) / 2; r++) {
                 int l1 = matrix[l][r];
                 int l2 = matrix[r][n - l];
                 int l3 = matrix[n - l][n - r];
@@ -39,4 +39,31 @@ public class Solution {
             }
         }
     }
+
+    //用翻转代替旋转 （水平翻转 + 主对角线翻转）
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length - 1 ;
+        //水平翻转
+        for( int i = 0 ; i < matrix.length  / 2; i++){
+            for(int j = 0 ; j < matrix.length ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-i][j] ;
+                matrix[n-i][j] = temp;
+            }
+        }
+
+        //主对角线翻转
+
+        for( int i = 0 ; i < matrix.length ; i++){
+            for ( int j = 0 ; j < matrix.length ; j++){
+                if( i == j ){
+                    break;
+                }
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i] ;
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
 }
