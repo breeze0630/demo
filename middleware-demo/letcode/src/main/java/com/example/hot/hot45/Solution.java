1fbec3c8b0aee4915752ea28e24c214148677c53
breeze0630 <external_ll@163.com>
2023-07-19 17:54:00 +0800

    添加新的问题

---
+++ middleware-demo/letcode/src/main/java/com/example/hot/hot45/Solution.java
@@ -0,0 +1,13 @@
+package com.example.hot.hot45;
+
+/**
+ * @description:
+ * @auther: liubiao
+ * @date: 2023/7/19
+ */
+public class Solution {
+    public int jump(int[] nums) {
+
+        return -1;
+    }
+}
