beabd63b8c902588dfb9f0c6c933bbac93a8531e
breeze0630 <external_ll@163.com>
2024-05-08 16:01:16 +0800

    添加stop watch

---
+++ middleware-demo/spring-learn-demo/other/src/main/java/com/example/springlearndemo/test/stopwatch/StopWatchTest.java
@@ -0,0 +1,26 @@
+package com.example.springlearndemo.test.stopwatch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
+
+/**
+ * @description:
+ * @auther: liubiao
+ * @date: 2024-05-08
+ */
+@Slf4j
+public class StopWatchTest {
+
+    public static void main(String[] args) throws Exception {
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start("测试1");
+
+        Thread.sleep(1);
+        stopWatch.stop();
+        log.info("\r\n" + stopWatch.prettyPrint());
+
+
+    }
+}
