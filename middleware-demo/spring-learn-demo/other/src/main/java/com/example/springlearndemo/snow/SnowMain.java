504abd4e3238c714c37a464bb51eb365df876877
breeze0630 <external_ll@163.com>
2024-05-28 08:34:29 +0800

    add testcase

---
+++ middleware-demo/spring-learn-demo/other/src/main/java/com/example/springlearndemo/snow/SnowMain.java
@@ -0,0 +1,15 @@
+package com.example.springlearndemo.snow;
+
+import java.util.UUID;
+
+/**
+ * @description:
+ * @auther: liubiao
+ * @date: 2024-05-28
+ */
+public class SnowMain {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
