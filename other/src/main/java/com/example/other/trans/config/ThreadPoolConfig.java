3f53f9a2b5a899b1f12058bf762d56a5e8308d75
breeze0630 <external_ll@163.com>
2024-06-21 20:30:31 +0800

    mofify ThreadPoolConfig

--- other/src/main/java/com/example/other/trans/config/ThreadPoolConfig.java
+++ other/src/main/java/com/example/other/trans/config/ThreadPoolConfig.java
@@ -21,13 +21,13 @@ public class ThreadPoolConfig
     private int corePoolSize = 50;
 
     // 最大可创建的线程数
-    private int maxPoolSize = 200;
+    private int maxPoolSize = 100;
 
     // 队列最大长度
     private int queueCapacity = 1000;
 
     // 线程池维护线程所允许的空闲时间
-    private int keepAliveSeconds = 300;
+    private int keepAliveSeconds = 500;
 
     @Bean(name = "threadPoolTaskExecutor")
     public ThreadPoolTaskExecutor threadPoolTaskExecutor()
