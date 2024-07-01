0d1baecff11af71243f7889777e37d134dc68455
breeze0630 <external_ll@163.com>
2024-07-01 13:54:12 +0800

    add day test

--- other/src/test/java/com/example/other/OtherApplicationTests.java
+++ other/src/test/java/com/example/other/OtherApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.other;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+
 
 @SpringBootTest
+@TestPropertySource("classpath:application.yml")
 class OtherApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("日期"+LocalDate.now());
     }
 
 }
