f09cd95edfc2edd1c59c08563f85fe3563df722b
breeze0630 <external_ll@163.com>
2024-07-01 16:28:48 +0800

    add day test

--- other/src/test/java/com/example/other/OtherApplicationTests.java
+++ other/src/test/java/com/example/other/OtherApplicationTests.java
@@ -14,7 +14,10 @@ class OtherApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("日期"+LocalDate.now());
+        System.out.println("日期 90 天:"+LocalDate.now().plusDays(-90));
+        System.out.println("日期 30 天:"+LocalDate.now().plusDays(-30));
+        System.out.println("日期 7 天:"+LocalDate.now().plusDays(-7));
+
     }
 
 }
