15837d4a68f989e44c08c9bc80ef20a94dc4579a
breeze0630 <external_ll@163.com>
2024-12-13 10:28:58 +0800

    添加 seata 核心实现的说明

--- middleware-demo/seata-demo/stock-service/src/main/java/com/breeze/sample/seata/stockservice/filter/HeaderFilter.java
+++ middleware-demo/seata-demo/stock-service/src/main/java/com/breeze/sample/seata/stockservice/filter/HeaderFilter.java
@@ -17,6 +17,11 @@ public class HeaderFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        /**
+         * seata 的全局事物的控制，就是通过 请求时，header 传递 TX_XID，默认设置为 存在TX_XID即使用现有xid，否则生成新的xid
+         * 在  SeataHandlerInterceptor 中 通过   RootContext.bind(rpcXid) 将 TX_XID 绑定到 RootContext.XID 中
+         * RootContext.getXID()
+         */
         String xid = ((HttpServletRequest)servletRequest).getHeader("TX_XID");
         log.info("HeaderFilter.xid:{}",xid);
         filterChain.doFilter(servletRequest, servletResponse);
